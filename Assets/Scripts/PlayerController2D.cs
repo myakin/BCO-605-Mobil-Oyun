278a897964d7c4fb4d97d7c12500d33a65905ce9
Murat Yakin <yakin_murat@yahoo.com>
2022-03-17 22:30:43 +0300

    jump probleminin cozumu

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -10,9 +10,11 @@ public class PlayerController2D : MonoBehaviour
     [SerializeField] private Animator animator;
     private SpriteRenderer targetRenderer;
     private float multiplier = 1;
-    private bool isJumping, isFalling;
+    private bool isJumping, isFalling, willFall;
     private Vector2 originalOffset, originalSize;
     private IEnumerator jumpResetCoroutine;
+    private float oldY;
+    
     
 
     private void Start() {
@@ -56,21 +58,35 @@ public class PlayerController2D : MonoBehaviour
         
         if (jump>0 && !isJumping) {
             isJumping = true;
-            animator.SetTrigger("jump");
-            
+            animator.SetFloat("moveMode",4);
+
             GetComponent<CapsuleCollider2D>().offset = new Vector2(originalOffset.x, 1.68f);
             GetComponent<CapsuleCollider2D>().size = new Vector2(originalSize.x, 1.53f);
             // GetComponent<Rigidbody2D>().simulated = false;
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce);
         }
 
-        // CheckIfJumpAnimationIsEnding();
-
-
-
         transform.position += transform.right * (hor * moveSpeed * multiplier);
         CheckGround();
-        
+        oldY = transform.position.y;
+    }
+
+    private void CheckGround() {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.down, 0.3f, 1<<31);
+        if (!hit) {
+            willFall = true;
+        }
+        if(isJumping && willFall && hit && transform.position.y<oldY) {
+            willFall = false;
+            animator.SetTrigger("fall");
+            ResetColliderSizeAndOffset();
+        }
+
+        if (hit && hit.transform.tag=="Platform") {
+            transform.SetParent(hit.transform);
+        } else {
+            transform.SetParent(null);
+        }   
     }
 
     public void ResetColliderSizeAndOffset() {
@@ -83,50 +99,10 @@ public class PlayerController2D : MonoBehaviour
         }
     }
 
-
     private IEnumerator Resetjump() {
         yield return new WaitForSeconds(0.2f);
         isJumping = false;
         animator.SetBool("proceedToJumpEnd", false);
         jumpResetCoroutine = null;
-    }
-
-
-    private void CheckIfJumpAnimationIsEnding() {
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("jump")) {
-            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime>0.9f) {
-                Debug.Log("jump is ending");
-                ResetColliderSizeAndOffset();
-            }
-        }
-    }
-
-
-    // private void OnCollisionEnter2D(Collision2D other) {
-    //     if(other.collider.tag=="Platform") {
-    //         transform.SetParent(other.transform);
-    //     }
-    // }
-
-
-
-    private void CheckGround() {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.down, 2f, 1<<31);
-        if (hit.distance>1f) {
-            isFalling = true;
-        } else {
-            if(isJumping && isFalling) {
-                isFalling = false;
-                animator.SetTrigger("fall");
-                ResetColliderSizeAndOffset();
-            }
-
-            if (hit && hit.transform.tag=="Platform") {
-                transform.SetParent(hit.transform);
-            } else {
-                transform.SetParent(null);
-            }
-            // isJumping = false;
-        }
     }
 }
